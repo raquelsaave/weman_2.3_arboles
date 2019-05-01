e7b17941f6336d82711f529a279ae1135f16a8f3
Raquel Saavedra <raquel_2496@hotmail.com>
2019-05-01 16:23:01 -0500

    Arreglo html

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ let ctrlMostradoT = document.getElementById('tiempo');
 function binarySearch(valor, personas) {
 	//var ahora= new Date();
 	var ahora= performance.now();
-  var valor = document.getElementById("txtBuscaR1").value
+  var valor = document.getElementById("txtBuscaR1").value;
   let primero = 0;
   let ultimo = personas.length - 1;
   let posicion = -1; let found = false; let medio;
@@ -29,16 +29,16 @@ function binarySearch(valor, personas) {
   //var despues= new Date();
   var despues= performance.now();
  // return personas[posicion].nombre;
-  imprime=personas[posicion].nombre
- imprimeI= ahora
+  	imprime=personas[posicion].nombre
+ 	imprimeI= ahora
  	imprimeF= despues
  	imprimeT= despues-ahora
 
   //console.log("arreglo",personas[posicion].nombre);
   //document.getElementById("Texto").innerHTML = personas[posicion].nombre;
-  ctrlMostrado.innerHTML = imprime;
-   ctrlMostradoI.innerHTML = imprimeI;
+  	ctrlMostrado.innerHTML = imprime;
+   	ctrlMostradoI.innerHTML = imprimeI;
     ctrlMostradoF.innerHTML = imprimeF;
-     ctrlMostradoT.innerHTML = imprimeT;
+    ctrlMostradoT.innerHTML = imprimeT;
   //return posicion;
 }
